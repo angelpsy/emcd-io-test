6ad65ffc3c911c5f83ff24c8afe498f06a03e9c4
AAlekseev <angelpsymail@gmail.com>
2023-11-30 16:54:19 +0400

    set up vue application

---
+++ vite.config.mjs
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    server: {
+      port: parseInt(process?.env?.VITE_DEV_SERVER_PORT ?? "3000"),
+    },
+  });
+};
